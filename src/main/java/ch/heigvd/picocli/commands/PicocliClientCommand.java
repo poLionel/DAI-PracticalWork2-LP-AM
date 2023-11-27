759a16f4c59abc8d1d6b4cf1cccd0d3c44000a5d
Lionel Pollien <lionel.pollien@heig-vd.ch>
2023-11-27 17:15:22 +0100

    picocli execute methods

--- src/main/java/ch/heigvd/picocli/commands/PicocliClientCommand.java
+++ src/main/java/ch/heigvd/picocli/commands/PicocliClientCommand.java
@@ -1,10 +1,31 @@
 package ch.heigvd.picocli.commands;
 
+import ch.heigvd.client.net.GameClient;
 import picocli.CommandLine;
+
 @CommandLine.Command(name = "Client", version = "client 0.1", mixinStandardHelpOptions = true)
 public class PicocliClientCommand implements Runnable{
 
     @Override
-    public void run(){}
+    public void run(){
+        boolean wasSuccessful = executeClient();
+    }
+
+    public boolean executeClient(){
+        try{
+            GameClient client = new GameClient("127.0.0.1");
+            Thread clientThread = new Thread(client::start);
+
+            clientThread.start();
+
+            clientThread.join();
+
+            return true;
+
+        }catch (Exception e){
+            //todo something
+            return false;
+        }
+    }
 
 }
--- src/main/java/ch/heigvd/picocli/commands/PicocliServerCommand.java
+++ src/main/java/ch/heigvd/picocli/commands/PicocliServerCommand.java
@@ -1,11 +1,29 @@
 package ch.heigvd.picocli.commands;
 
+import ch.heigvd.server.net.GameServer;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "Server", version = "server 0.1", mixinStandardHelpOptions = true)
 public class PicocliServerCommand implements Runnable{
 
     @Override
-    public void run(){}
+    public void run() {
+        boolean wasSuccessfull = executeServer();
+    }
 
+    public boolean executeServer(){
+        try{
+            GameServer server = new GameServer();
+            Thread serverThread = new Thread(server::start);
+
+            serverThread.start();
+
+            return true;
+
+        }catch (Exception e){
+            //todo something
+            return false;
+        }
+
+    }
 }
